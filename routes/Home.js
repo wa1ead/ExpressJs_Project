61231701f695903c7f9e670386939919d0e7384b
wa1ead <waliddraa2@gmail.com>
2024-10-25 12:41:44 +0100

    creating home view

--- routes/Home.js
+++ routes/Home.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+
+const publicPath = path.join(__dirname, "views");
 
 router.get("/", (req, res) => {
-  res.send("HOME");
+  res.sendFile(`${publicPath}/Home.html`);
 });
 
 module.exports = router;
