f9debbde1c50c469932b92eafd6a449a46261a10
wa1ead <waliddraa2@gmail.com>
2024-10-25 12:44:46 +0100

    creating contact view

--- routes/Contact.js
+++ routes/Contact.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+
+const publicPath = path.join(__dirname, "views");
 
 router.get("/", (req, res) => {
-  res.send("CONTACT");
+  res.sendFile(`${publicPath}/Contact.html`);
 });
 
 module.exports = router;
