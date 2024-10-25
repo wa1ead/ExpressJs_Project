2b6e3b068d2ef33c07c6ee28a4905af3bbf7b8e3
wa1ead <waliddraa2@gmail.com>
2024-10-25 09:37:32 +0100

    generating services route

--- routes/Services.js
+++ routes/Services.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("HOME");
+  res.send("SERVICES");
 });
 
 module.exports = router;
