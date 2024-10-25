dfcd494e04e50fd6a491c61bb9b3f1973545d24d
wa1ead <waliddraa2@gmail.com>
2024-10-25 09:34:16 +0100

    generating home route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const PORT = 3030
+const express = require("express");
+const app = express();
+const PORT = 3030;
 
-app.listen(PORT, ()=>{
-  console.log('Server is running on PORT: '${PORT})
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+});
