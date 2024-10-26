728c55888066b4ce6b469505015dd4734a8f350f
wa1ead <waliddraa2@gmail.com>
2024-10-26 17:34:06 +0100

    setting workingTime middleware

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const homeRoute = require("./routes/Home");
 const servicesRoute = require("./routes/Services");
 const contactRoute = require("./routes/Contact");
 
+const workingTime = (req, res, next) => {
+  var hour = new Date().getHours();
+  var day = new Date().getDay();
+
+  if (day != 0 && day != 6 && hour >= 9 && hour <= 17) {
+    console.log("opened");
+    next();
+  } else {
+    console.log("closed");
+    res.send("<h1>closed</h1>");
+  }
+};
+
+app.use(workingTime);
 app.use("/home", homeRoute);
 app.use("/services", servicesRoute);
 app.use("/contact", contactRoute);
